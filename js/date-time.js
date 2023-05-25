68e73bddce6573fcbf4d431a927f267c5cb0e996
Obua Denis Daniel <dd.obua@gmail.com>
2023-05-25 16:17:47 +0300

    Create dates in different ways

--- js/date-time.js
+++ js/date-time.js
@@ -1,1 +1,12 @@
 'use strict';
+
+// Create a date
+const now = new Date();
+console.log(now);
+
+console.log(new Date('2023-05-25T12:53:09.247Z'));
+console.log(new Date('August 21, 2010'));
+console.log(new Date('2019-11-18T21:31:17.178Z'));
+console.log(new Date(2013, 11, 15, 16, 55, 30));
+console.log(new Date(0));
+console.log(new Date(3 * 24 * 60 * 60 * 1000));
