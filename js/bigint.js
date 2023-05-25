4d89ab1f39d2ca979f2a54095ef270cfb4408aa3
Obua Denis Daniel <dd.obua@gmail.com>
2023-05-25 15:04:07 +0300

    Make computations using big ints

--- js/bigint.js
+++ js/bigint.js
@@ -5,3 +5,10 @@ console.log('Maximum safe integer:', Number.MAX_SAFE_INTEGER);
 
 console.log(117890986523294701283740185689032834820623461928364n);
 console.log(BigInt(86856430298745));
+
+// Make computations using big ints
+console.log(10000000n + 10000000n);
+console.log(10000000n * 10000000n);
+const huge = 49378075689405286352838456028n;
+const num = 23;
+console.log(huge * BigInt(num));
