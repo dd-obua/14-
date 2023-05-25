357281fc4c78a17afde628574c9324a17f79dc3b
Obua Denis Daniel <dd.obua@gmail.com>
2023-05-25 11:41:37 +0300

    Find the minimum and maximum values of numbers

--- js/math-rounding.js
+++ js/math-rounding.js
@@ -3,3 +3,6 @@
 console.log(Math.sqrt(25));
 console.log(25 ** (1 / 2));
 console.log(8 ** (1 / 3));
+
+console.log(Math.max(2, 3, 53, 5, 29, 2, 14));
+console.log(Math.min(2, 3, 53, 5, 29, 2, 14));
