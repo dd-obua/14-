19d27de7dd0e5361b6cbf13e897d44cb85ec216d
Obua Denis Daniel <dd.obua@gmail.com>
2023-05-25 16:27:54 +0300

    Apply date methods on generated date

--- js/date-time.js
+++ js/date-time.js
@@ -10,3 +10,15 @@ console.log(new Date('2019-11-18T21:31:17.178Z'));
 console.log(new Date(2013, 11, 15, 16, 55, 30));
 console.log(new Date(0));
 console.log(new Date(3 * 24 * 60 * 60 * 1000));
+console.log();
+
+const future = new Date(2025, 11, 15, 16, 55, 43);
+console.log(future);
+console.log(future.getFullYear());
+console.log(future.getMonth() + 1);
+console.log(future.getDate());
+console.log(future.getDay());
+console.log(future.getHours());
+console.log(future.getMinutes());
+console.log(future.getSeconds());
+console.log(future.toISOString());
