7b522e0448add85010f72c48cf72630383674735
Obua Denis Daniel <dd.obua@gmail.com>
2023-05-25 11:46:57 +0300

    Generate random integers [1-6]

--- js/math-rounding.js
+++ js/math-rounding.js
@@ -8,3 +8,5 @@ console.log(Math.max(2, 3, 53, 5, 29, 2, 14));
 console.log(Math.min(2, 3, '53', 5, 29, 2, 14));
 
 console.log(Math.PI * Number.parseFloat('10px') ** 2);
+
+console.log(Math.trunc(Math.random() * 6) + 1);
