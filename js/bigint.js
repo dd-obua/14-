17b320f0b208ee56e2387fefcf46aa841950bbf0
Obua Denis Daniel <dd.obua@gmail.com>
2023-05-25 15:00:57 +0300

    Create big integer values

--- js/bigint.js
+++ js/bigint.js
@@ -2,3 +2,6 @@
 
 console.log('Maximum num:', 2 ** 53 - 1);
 console.log('Maximum safe integer:', Number.MAX_SAFE_INTEGER);
+
+console.log(117890986523294701283740185689032834820623461928364n);
+console.log(BigInt(86856430298745));
