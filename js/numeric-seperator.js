7be35449dc742970eb34eca22e7d40f208df80d0
Obua Denis Daniel <dd.obua@gmail.com>
2023-05-25 14:16:07 +0300

    Show behavior of numeric seperator when converting from a string

--- js/numeric-seperator.js
+++ js/numeric-seperator.js
@@ -5,3 +5,11 @@ console.log(solarDiameter * 2);
 
 const priceCents = 345_99;
 console.log(priceCents);
+
+const transferFee1 = 15_00;
+const transferFee2 = 1_500;
+console.log(transferFee1);
+console.log(transferFee2);
+
+console.log(Number('230_000'));
+console.log(Number.parseInt('230_000'));
