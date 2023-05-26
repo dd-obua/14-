ed5028a733cba845c411d4f573c4e70b592c281b
Obua Denis Daniel <dd.obua@gmail.com>
2023-05-26 19:34:09 +0300

    Provide options to the Number format method

--- js/intl-numbers.js
+++ js/intl-numbers.js
@@ -1,7 +1,15 @@
 'use strict';
 
+const options = {
+  style: 'unit',
+  unit: 'mile-per-hour',
+};
+
 const num = 8403492.23;
-console.log('US:', new Intl.NumberFormat('en-US').format(num));
-console.log('Germany:', new Intl.NumberFormat('de-DE').format(num));
-console.log('Syria:', new Intl.NumberFormat('ar-SY').format(num));
-console.log('Browser:', new Intl.NumberFormat(navigator.language).format(num));
+console.log('US:', new Intl.NumberFormat('en-US', options).format(num));
+console.log('Germany:', new Intl.NumberFormat('de-DE', options).format(num));
+console.log('Syria:', new Intl.NumberFormat('ar-SY', options).format(num));
+console.log(
+  'Browser:',
+  new Intl.NumberFormat(navigator.language, options).format(num)
+);
