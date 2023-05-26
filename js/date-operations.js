ce0ff2bb430e7a06d45f9f2e8dee647d5eae412c
Obua Denis Daniel <dd.obua@gmail.com>
2023-05-26 11:51:34 +0300

    Define calcDaysPassed function

--- js/date-operations.js
+++ js/date-operations.js
@@ -4,3 +4,6 @@ const future = new Date(2037, 10, 19, 15, 23);
 console.log(future);
 console.log(Number(future));
 console.log(+future);
+
+const calcDaysPassed = (date1, date2) =>
+  (date2 - date1) / (24 * 60 * 60 * 1000);
