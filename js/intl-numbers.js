97bedb2815d3dd1af9b6ca250a7d6ab2c4b2ff32
Obua Denis Daniel <dd.obua@gmail.com>
2023-05-26 19:39:53 +0300

    Apply options 2

--- js/intl-numbers.js
+++ js/intl-numbers.js
@@ -5,6 +5,11 @@ const options = {
   unit: 'mile-per-hour',
 };
 
+const options2 = {
+  style: 'currency',
+  currency: 'EUR',
+};
+
 const num = 8403492.23;
 console.log('US:', new Intl.NumberFormat('en-US', options).format(num));
 console.log('Germany:', new Intl.NumberFormat('de-DE', options).format(num));
@@ -12,4 +17,13 @@ console.log('Syria:', new Intl.NumberFormat('ar-SY', options).format(num));
 console.log(
   'Browser:',
   new Intl.NumberFormat(navigator.language, options).format(num)
+);
+console.log();
+
+console.log('US:', new Intl.NumberFormat('en-US', options2).format(num));
+console.log('Germany:', new Intl.NumberFormat('de-DE', options2).format(num));
+console.log('Syria:', new Intl.NumberFormat('ar-SY', options2).format(num));
+console.log(
+  'Browser:',
+  new Intl.NumberFormat(navigator.language, options2).format(num)
 );
