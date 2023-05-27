eab5b9273616ae2820b8954baf693bcf18e319b1
Obua Denis Daniel <dd.obua@gmail.com>
2023-05-27 11:16:18 +0300

    Provide arguments as destructured arrays

--- js/settimeout.js
+++ js/settimeout.js
@@ -1,10 +1,10 @@
 'use strict';
 
+const ingredients = ['olives', 'spinach'];
 setTimeout(
   (ingred1, ingred2) =>
     console.log(`Here is your ${ingred1} and ${ingred2} pizza.`),
   3000,
-  'olives',
-  'spinach'
+  ...ingredients
 );
 console.log('Waiting ...');
